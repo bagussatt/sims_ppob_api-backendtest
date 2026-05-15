8995f74e027f3c1e5a0255bda62c4c80dc8f781a
Bagus Satrio <115222053+bagussatt@users.noreply.github.com>
2026-05-15 14:34:44 +0700

    `Update swagger.js configuration with minor changes`

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,30 +1,30 @@
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'SIMS PPOB API - Bagus Satrio',
-      version: '1.0.0',
-      description: 'API Documentation sesuai kontrak Nutech Integrasi',
+      title: "SIMS PPOB API - Bagus Satrio",
+      version: "1.0.1",
+      description: "API Documentation sesuai kontrak Nutech Integrasi",
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}`,
-        description: 'Development server',
+        url: process.env.APP_URL || "http://localhost:3000",
+        description: "Dynamic Server",
       },
     ],
     components: {
       securitySchemes: {
         bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
     },
   },
-  apis: ['./src/routes/*.js'],
+  apis: ["./src/routes/*.js"],
 };
 
 module.exports = swaggerJsdoc(options);
