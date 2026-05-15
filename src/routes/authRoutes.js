5f5f852338c8070859db7d04460f7b034f337c47
Bagus Satrio <115222053+bagussatt@users.noreply.github.com>
2026-05-15 14:30:44 +0700

    "refactor: Updated authRoutes.js to use uploadMiddleware instead of uploadMIddleware"

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
-const upload = require("../middlewares/uploadMIddleware");
+const upload = require("../middlewares/uploadMiddleware");
 
 /**
  * @swagger
